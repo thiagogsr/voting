dbefcdf8f1fd521dcdfe7b250cdc44386903f9d0
Thiago Guimaraes <thiagogsr@gmail.com>
2020-02-15 16:49:02 -0300

    Install credo

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule Voting.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.2", only: [:dev, :test], runtime: false}
     ]
   end
 
